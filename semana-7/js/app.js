bcf91cae4ea5da4c7a6bccac156bfa49d98152b4
aron15951 <aronchoque72@gmail.com>
2024-03-18 18:41:52 -0500

    e-comerce-and-init-sem-7

---
+++ semana-7/js/app.js
@@ -0,0 +1,8 @@
+const containerBill = document.querySelector("#contailer-bill");
+const containerPercentages = document.querySelector("#container-percentage");
+const inputBill = document.querySelector("#input-bill");
+
+// click
+containerBill.onclick = function () {
+  inputBill.focus();
+};
